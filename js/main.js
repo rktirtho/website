d0421343cbebf1d6e451adce47786a47c447ac2e
lifeoflikhon <contact@lifeoflikhon.com>
2019-09-01 02:52:51 +0600

    created homepage

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Footer to_year
+document.querySelector('.to_year').textContent = new Date().getFullYear();
+
+// Version
+document.querySelector('.version').textContent = version;
+
+// Count
+document.querySelector('.members_count').textContent = membersCount;
+document.querySelector('.events_count').textContent = eventsCount;
+document.querySelector('.projects_count').textContent = projectsCount;
+document.querySelector('.contests_count').textContent = contestsCount;
+
+// Notice
+document.querySelector('.next_meeting').textContent = nextMeetingDate;
---
+++ js/only__modify__it.js
@@ -0,0 +1,29 @@
+// Version
+let version = '1.0.0';
+
+// Count
+let membersCount = 20;
+let eventsCount = 0;
+let projectsCount = 0;
+let contestsCount = 0;
+
+// Notice
+let noticeTitle = 'notice title';
+let noticeBody = 'notice body';
+let nextMeetingDate = 'September 03, 2019 5:15PM at Room No. 203';
+
+// Admins
+let admins = [];
+let adminInfo = {
+  id: 'id',
+  name: 'name1',
+  github: 'github__username'
+};
+
+// Members
+let members = [];
+let userInfo = {
+  id: 'id',
+  name: 'name1',
+  github: 'github__username'
+};
